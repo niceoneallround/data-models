d4cdd7ac1f43695a2e7f988a89a423ecca0c12ae
niceoneallround <niceoneallround@gmail.com>
2016-02-24 22:00:27 -0800

    Add a promise for fetching a context by its data model

--- test/jsonld-contexts/testUtils.js
+++ test/jsonld-contexts/testUtils.js
@@ -2,7 +2,10 @@
 
 var assert = require('assert'),
   contextUtils = require('../../lib/jsonld-contexts/utils'),
+  jsonldUtils = require('jsonld-utils/lib/jldUtils'),
   PNDataModel = require('../../lib/PNDataModel'),
+  PN_P = PNDataModel.PROPERTY,
+  PN_T = PNDataModel.TYPE,
   should = require('should'),
   util = require('util');
 
@@ -10,13 +13,66 @@ describe('JSONLD Context utils tests', function () {
   'use strict';
 
   describe('1 Read context tests', function () {
-    it('1.1 should read PNDataModel context', function (done) {
-      contextUtils.getContext(PNDataModel.ID, function (err, context) {
-        assert(!err, util.format('Error:%j reading context', err));
-        assert(context, util.format('no context read for datamodel:%s', PNDataModel.ID));
-        context.should.have.property('@context');
-        done();
-      });
+
+    it('1.1 should read PNDataModel context', function () {
+
+      var getContextPromise = contextUtils.getContextPromise(PNDataModel.ID);
+
+      // return promise from catch as mocha will check if ok or error
+      return getContextPromise.then(
+        function (context) {
+          assert(context, util.format('then - no context read for datamodel:%s - context:%j', PNDataModel.ID, context));
+          context.should.have.property('@context');
+        },
+
+        function (err) {
+          throw new Error(err);
+        }
+      );
+    }); // 1.1
+  }); // describe 1
+
+  describe('2 Read context, and use in extracting information from a PnDataModel jsonld graph', function () {
+
+    it('2.1 should read PNDataModel context', function () {
+
+      var getContextPromise = contextUtils.getContextPromise(PNDataModel.ID),
+          testObject;
+
+      testObject = {
+        '@id': 'http://id.webshield.io/request/acme/com#1',
+        '@type': [PN_T.CreateCollection] };
+      testObject[PN_P.domain] = { '@id': 'http://id.pn.webshield.io/domain#1' };
+      testObject[PN_P.dataModel] = { '@id': 'http://id.pn.webshield.io/datamodel#1' };
+
+      console.log('testObject:%j', testObject);
+
+      // return promise from catch as mocha will check if ok or error
+      return getContextPromise.then(
+        function (context) {
+          var findObjectPromise;
+          assert(context, util.format('then - no context read for datamodel:%s - context:%j', PNDataModel.ID, context));
+
+          findObjectPromise = jsonldUtils.findObjectsPromise(
+                                          testObject,
+                                          PN_T.CreateCollection,
+                                          new Map().set('@context', context));
+
+          findObjectPromise.then(
+            function (result) {
+              console.log('result:%j', result);
+            },
+
+            function (reason) {
+              console.log('reason:%j', reason);
+            }
+          );
+        },
+
+        function (err) {
+          throw new Error(err);
+        }
+      );
     }); // 1.1
   }); // describe 1
 });
